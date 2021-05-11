c9e231bf5f8432c95bec358f86856b5e7127823c
em-lime <68914515+lime-n@users.noreply.github.com>
2021-05-11 19:54:02 +0100

    Add files via upload

--- Ocean Script/Ocean data.R
+++ Ocean Script/Ocean data.R
@@ -70,14 +70,18 @@ plot(oxy.agg,
 #[1]    0.0000  100.8374  201.6748  302.5122  403.3496  504.1870  605.0244  705.8618  806.6992  907.5366 1008.3740
 myIntervals <- c("0 - 100", "100 - 200", "200 - 300", "300 - 400","400 - 500","500 - 600","600 - 700","700 - 800","800 - 900","900 - 1010")
 dataset$depth_range<- myIntervals[findInterval(dataset$depth, c(0, 100.8374, 201.6748, 302.5122, 403.3496, 504.1870, 605.0244, 705.8618, 806.6992, 907.5366, 1008.3740))]
-
+dataset$depth_range <- as.factor(dataset$depth_range)
 #oxygen range
 seq(0, 209.5374, 20.95374)
 # [1]   0.00000  20.95374  41.90748  62.86122  83.81496 104.76870 125.72244 146.67618 167.62992 188.58366 209.53740
 myIntervals <- c("0 - 21", "21 - 41", "41 - 61", "61 - 81","81 - 101","101 - 121","121 - 141","141 - 161","161 - 181","181 - 201", "201-211")
 dataset$oxygen_range<- myIntervals[findInterval(dataset$z, c(0, 20.95374, 41.90748, 62.86122, 83.81496, 104.76870, 125.72244, 146.67618, 167.62992, 188.58366, 209.53740))]
+dataset$oxygen_range <- as.factor(dataset$oxygen_range)
+#fix the wrong order
+dataset$oxygen_range <- factor(dataset$oxygen_range, levels=levels(dataset$oxygen_range)[order(as.numeric(gsub("( -.*)", "", levels(dataset$oxygen_range))))])
+d <- d[order(d$x2), ]
 
-#count the frequency by their ranges
+#count the frequency by their ranges; To test the distribution of data collected from the glider
 
 dataset %>% count(depth_range, oxygen_range, day) %>% group_by(day)
 #glimpse
@@ -96,6 +100,68 @@ dataset %>% count(depth_range, oxygen_range, day) %>% group_by(day)
 #9 0 - 100     101 - 121       10   940
 #10 0 - 100     101 - 121       11   611
 # ... with 322 more rows
+
+
+ranges <- data.frame(ranges)
+#arrange order
+ranges$oxygen_range <- factor(ranges$oxygen_range, levels=levels(ranges$oxygen_range)[order(as.numeric(gsub("( -.*)", "", levels(ranges$oxygen_range))))])
+
+str(ranges)
+#str(ranges)
+#'data.frame':	332 obs. of  4 variables:
+#  $ depth_range : chr  "0 - 100" "0 - 100" "0 - 100" "0 - 100" ...
+#$ oxygen_range: chr  "101 - 121" "101 - 121" "101 - 121" "101 - 121" ...
+#$ day         : int  2 3 4 5 6 7 8 9 10 11 ...
+#$ n           : int  416 566 440 459 753 612 643 499 940 611 ...
+
+#factorise the character data
+ranges[,1:2] <- lapply(ranges[,1:2], as.factor)
+
+#fit onto a multiple-linear regression after fitting hist for normality; data shows some partial skewness though we'll fit  the mlm anyways
+library(olsrr)
+hist(sqrt(ranges$n))
+#mlm
+mlm.dataset <- lm(log(n)~depth_range+oxygen_range+day, ranges)
+#stepforward
+step.dataset <- ols_step_forward_p(mlm.dataset, p.enter = 0.05)
+summary(step.dataset$model)
+
+#Linear model shows that as the number of counts collected from the glider for increase increases, then it declines as the days progress
+#A lot of negative estimates, perhaps related towards the decline in accuracy and data-collection from the float over-time
+#Counts increase with depth, though increase in counts have a potential decrease in oxygen increase?
+#Though a better test may be necessary given that normality was weak, and further assumptions will need to be considered like colinearity between variables.
+
+#Coefficients:
+
+#  Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)            5.82102    0.39905  14.587  < 2e-16 ***
+#  oxygen_range21 - 41   -1.58408    0.23673  -6.692 1.03e-10 ***
+#  oxygen_range41 - 61   -1.64861    0.35243  -4.678 4.33e-06 ***
+#  oxygen_range61 - 81   -1.28990    0.35330  -3.651 0.000306 ***
+#  oxygen_range81 - 101  -0.86208    0.35433  -2.433 0.015537 *  
+#  oxygen_range101 - 121 -0.42899    0.35118  -1.222 0.222804    
+#oxygen_range121 - 141 -0.11510    0.48074  -0.239 0.810930    
+#oxygen_range141 - 161  0.63774    0.48074   1.327 0.185614    
+#oxygen_range161 - 181 -1.39896    0.48074  -2.910 0.003875 ** 
+#  oxygen_range181 - 201 -2.54797    0.48074  -5.300 2.20e-07 ***
+#  oxygen_range201-211   -5.66754    1.24722  -4.544 7.90e-06 ***
+#  depth_range100 - 200   0.66111    0.28620   2.310 0.021544 *  
+#  depth_range200 - 300   0.19288    0.39770   0.485 0.628025    
+#depth_range300 - 400   1.84981    0.48074   3.848 0.000145 ***
+#  depth_range400 - 500   1.76826    0.48074   3.678 0.000277 ***
+#  depth_range500 - 600   1.74791    0.48074   3.636 0.000324 ***
+#  depth_range600 - 700   1.77561    0.48074   3.694 0.000261 ***
+#  depth_range700 - 800   0.14827    0.41936   0.354 0.723904    
+#depth_range800 - 900   1.13086    0.44863   2.521 0.012213 *  
+#  depth_range900 - 1010  2.25778    0.49681   4.545 7.89e-06 ***
+#  day                   -0.03837    0.01525  -2.516 0.012389 *  
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#Residual standard error: 1.188 on 311 degrees of freedom
+#Multiple R-squared:  0.534,	Adjusted R-squared:  0.504 
+#F-statistic: 17.82 on 20 and 311 DF,  p-value: < 2.2e-16
+
 
 #potential to try:
 
